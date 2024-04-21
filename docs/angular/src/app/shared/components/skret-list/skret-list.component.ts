feb03935d3f89f9c7f9e83d24724063125b2691b
laraanzur <lara.anzur@gmail.com>
2024-04-21 06:58:44 +0200

    added details to list, calculated distance

--- docs/angular/src/app/shared/components/skret-list/skret-list.component.ts
+++ docs/angular/src/app/shared/components/skret-list/skret-list.component.ts
@@ -17,8 +17,11 @@ export class SkretListComponent implements OnInit{
   ) {}
 
   skreti!: Skret[];
+  closestSkreti!: Skret[];
   filteredSkreti!: Skret[];
   selectedSkret!: Skret;
+  currLat = '';
+  currLon = '';
 
   sidebarCollapsed = false;
 
@@ -29,9 +32,51 @@ export class SkretListComponent implements OnInit{
 
   ngOnInit(): void {
     this.getToilets();
+    this.getLocation();
   }
 
   getToilets(): void {
-    this.appDataService.getToilets().subscribe(skreti => this.skreti = skreti);
+    this.appDataService.getToilets().subscribe((data: any) => {
+      this.skreti = data; 
+      this.closestSkreti = data;
+    });
   }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          this.currLat = latitude.toString();
+          this.currLon = longitude.toString();
+          this.computeDistances();
+
+          // You can now use latitude and longitude in your application
+          // For example, display them on a map or send to a server
+        },
+        (error) => {
+          console.error("Error getting location:", error.message);
+        }
+      );
+    } else {
+      console.log("Geolocation is not available.");
+    }
+  }
+
+  computeDistances(){
+    var distances = [];
+    for (let s of this.skreti){
+      var dist = Math.abs(s.lat - parseFloat(this.currLat)) + Math.abs(s.lon - parseFloat(this.currLon));
+      distances.push(dist)
+    }
+
+    const indexedFloats = distances.map((value, index) => ({ value, index }));
+    indexedFloats.sort((a, b) => a.value - b.value);
+    const lowest10Floats = indexedFloats.slice(0, 10);
+
+    const selectedObjects = lowest10Floats.map(item => this.skreti[item.index]);
+    this.closestSkreti = selectedObjects;
+  }
+
 }
