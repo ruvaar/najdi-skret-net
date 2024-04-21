71468110cf818355c72b0b019b588e1a9d32d058
ruvaar <ra7285@student.uni-lj.si>
2024-04-21 02:45:39 +0200

    shown markers

--- docs/angular/src/app/shared/components/maps/maps.component.ts
+++ docs/angular/src/app/shared/components/maps/maps.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { AppDataService } from '../../services/app-data.service';
 
 
 @Component({
@@ -16,18 +17,21 @@ import { map } from 'rxjs/operators';
   ]
 })
 
-
- 
 export class MapsComponent implements OnInit {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private appService: AppDataService
+  ) { }
   
+  private mapp!: L.Map;
 
   ngOnInit() {
     this.initMap();
+    this.showSkreti();
   }
   
   private initMap(): void {
     var map = L.map('map').setView([46.0569, 14.5058], 13);
+    this.mapp = map;
   
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -36,6 +40,17 @@ export class MapsComponent implements OnInit {
     L.marker([46.0569, 14.5058]).addTo(map)
       .bindPopup('Poglej ga no, hot skret v tvoji bljizini!')
       .openPopup();
+  }
+
+  private showSkreti(): void {
+    this.appService.getToilets().subscribe((data: any) => {
+      for (let d of data) {
+        L.marker([d.lat, d.lon]).addTo(this.mapp)
+          .bindPopup(d.location)
+          .openPopup();
+      } 
+
+    });
   }
 
 }
